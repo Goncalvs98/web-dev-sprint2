2a276be2a96f0c7d494886f2f5510a3f80c147ac
Gabriel Gonçalves <52613723+Goncalvs98@users.noreply.github.com>
2024-11-02 20:01:16 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import FeaturesScreen from './screens/FeaturesScreen';
+import NewsletterScreen from './screens/NewsletterScreen';
+import AboutScreen from './screens/AboutScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Features" component={FeaturesScreen} />
+        <Stack.Screen name="Newsletter" component={NewsletterScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
